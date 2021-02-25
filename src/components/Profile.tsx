a34e59ab67aa124d8dfd88243737a13e20eda6d6
Bruno Weber <contato@brunoweber.com.br>
2021-02-25 16:54:23 -0300

    Added CountdownContext and improved user experience
    CountdownContext has been added to separate responsibilities for the countdown clock. What was Business Rule went to Contexts and what was responsible for Visualizing and handling numbers went to Components. In addition, visualization for the user was performed, such as notification and sound when a new challenge was launched.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,20 @@
-import styles from '../styles/components/Profile.module.css'
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
-      <img src="http://github.com/brunohsweber.png" alt="Foto de Bruno Weber"/>
+      <img src="http://github.com/brunohsweber.png" alt="Foto de Bruno Weber" />
       <div>
         <strong>Bruno Weber</strong>
         <p>
-          <img src="icons/level.svg" alt="Level"/>
-          Level 1</p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,34 @@
-import Head from 'next/head'
+import Head from "next/head";
 import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from '../components/Profile';
-import { CompletedChallenges } from '../components/CompletedChallenges';
-import { Countdown } from '../components/Countdown';
-import { ChallengeBox } from '../components/ChallengeBox';
+import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { CountdownProvider } from "../contexts/CountdownContext";
+import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from "../components/ChallengeBox";
 
-
-import styles from '../styles/pages/Home.module.css'
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
       <Head>
         <title>Início | move.it</title>
       </Head>
 
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
-      
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
-  )
+  );
 }
