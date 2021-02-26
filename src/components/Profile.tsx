7601f8212319b3399d95db23ba458f8a0620c301
Bruno Weber <contato@brunoweber.com.br>
2021-02-26 17:01:17 -0300

    Changed user avatar object-fit to cover

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -8,7 +8,7 @@ export function Profile() {
   return (
     <div className={styles.profileContainer}>
       <img
-        src="https://images.unsplash.com/photo-1514543250559-83867827ecce?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1311&q=80"
+        src="https://images.unsplash.com/photo-1514543250559-83867827ecce?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=cover&w=1311&q=80"
         alt="Foto do avatar"
       />
       <div>
