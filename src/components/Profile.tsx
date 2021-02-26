1a00522ee99e210cae60473c2a7418f1b593ee5d
Bruno Weber <contato@brunoweber.com.br>
2021-02-26 16:57:36 -0300

    Change default user name

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,9 +7,12 @@ export function Profile() {
 
   return (
     <div className={styles.profileContainer}>
-      <img src="http://github.com/brunohsweber.png" alt="Foto de Bruno Weber" />
+      <img
+        src="https://images.unsplash.com/photo-1514543250559-83867827ecce?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1311&q=80"
+        alt="Foto do avatar"
+      />
       <div>
-        <strong>Bruno Weber</strong>
+        <strong>Nome do usuário</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
           Level {level}
